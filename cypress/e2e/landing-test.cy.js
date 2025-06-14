34ab09ba82c6d3855151d95520801dd67b5f9c1b
Danish Mustafa <mdanishmustafa98@gmail.com>
2025-06-14 14:46:38 +0500

    Updated test cases

--- cypress/e2e/landing-test.cy.js
+++ cypress/e2e/landing-test.cy.js
@@ -1,34 +1,31 @@
-
 import LandingPage from "../support/pages/landing-page";
 
-describe('Landing page test suite', () => {
-    const landingPage = new LandingPage();
-    let landingPageTestData;
+describe("Landing page test suite", () => {
+  const landingPage = new LandingPage();
+  let landingPageTestData;
 
-    beforeEach(() => {
-        // Visit the landing page before running tests
-        landingPage.visit();
-        
-        // Load test data from fixture file
-        cy.fixture("landing-page-data").then((testData) => {
-            landingPageTestData = testData;
-        });
+  beforeEach(() => {
+    // Visit the landing page before running tests
+    landingPage.visit();
+
+    // Load test data from fixture file
+    cy.fixture("landing-page-data").then((testData) => {
+      landingPageTestData = testData;
     });
+  });
 
-   
+  // Verify sees advance search widget
 
- // Verify sees advance search widget 
+  it("Verify user can see advance search widget on landing page", () => {
+    landingPage.advanceSearchWidget().should("be.visible");
+  });
 
- it('Verify user can see advance search widget on landing page', () => {
-    landingPage.advanceSearchWidget().should('be.visible')
- })
-
-    // Verify navigation of user to the listing page on location selection
-    it("Verify user navigation on location selection", () => {
-        landingPage.typeLocation(landingPageTestData.propertyLocation);
-        cy.url().should('include', `/listings/city/${landingPageTestData.propertyLocation}`);
-    });
-
-
-
-})
+  // Verify navigation of user to the listing page on location selection
+  it("Verify user navigation on location selection", () => {
+    landingPage.typeLocation(landingPageTestData.propertyLocation);
+    cy.url().should(
+      "include",
+      `/listings/city/${landingPageTestData.propertyLocation}`
+    );
+  });
+});
